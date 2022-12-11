ad60ec2fe4de0c8207bbe6fa3e0c6c1b9e43a9cf
saleh asadi <salehasadi1991@gmail.com>
2022-12-11 10:35:52 +0330

    asd

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import AudioPlayer from "react-h5-audio-player";
 import "react-h5-audio-player/lib/styles.css";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 // import 'react-h5-audio-player/lib/styles.less' Use LESS
 // import 'react-h5-audio-player/src/styles.scss' Use SASS
 
@@ -11,60 +11,79 @@ const player = () => {
   <></>;
 };
 export default function Home() {
-  useEffect(() => {
-    // @ts-ignore
-    document.querySelector("audio").play();
-  }, []);
+  const [onPlay, setOnplay] = useState(false);
+  useEffect(() => {}, []);
   return (
     <>
       <audio src="musics/s.mp3" />
-      <div className={styles.container}>
-        <div className="wrapper">
-          <div className="border-circle" id="one"></div>
-          <div className="border-circle" id="two"></div>
-          <div className="background-circle">
-            <div className="triangle-light"></div>
-            <div className="body"></div>
-            <span className="shirt-text">M</span>
-            <span className="shirt-text">♥</span>
-            <span className="shirt-text">R</span>
-            <span className="shirt-text">Y</span>
-            <span className="shirt-text">AM</span>
-            <div className="triangle-dark"></div>
-          </div>
-          <div className="head">
-            <div className="ear" id="left"></div>
-            <div className="ear" id="right"></div>
-            <div className="hair-main">
-              <div className="sideburn" id="left"></div>
-              <div className="sideburn" id="right"></div>
-              <div className="hair-top"></div>
-            </div>
-            <div className="face">
-              <div className="hair-bottom"></div>
-              <div className="nose"></div>
-              <div className="eye-shadow" id="left">
-                <div className="eyebrow"></div>
-                <div className="eye"></div>
-              </div>
-              <div className="eye-shadow" id="right">
-                <div className="eyebrow"></div>
-                <div className="eye"></div>
-              </div>
-              <div className="mouth"></div>
-              <div className="shadow-wrapper">
-                <div className="shadow"></div>
-              </div>
-            </div>
-          </div>
-          <span className="music-note" id="one">
-            &#9835;
-          </span>
-          <span className="music-note" id="two">
-            &#9834;
-          </span>
+      {!onPlay && (
+        <div
+          className="button"
+          onClick={() => {
+            // @ts-ignore
+            document.querySelector("audio").play();
+            setOnplay(true);
+          }}
+        >
+          <h3>Hi Maryam</h3>
+          <p>Please Click On Play Button Azizooom</p>
+          <Image
+            src="/images/button-308744__340.webp"
+            alt=""
+            width={120}
+            height={130}
+          />
         </div>
-      </div>
+      )}
+      {onPlay && (
+        <div className={styles.container}>
+          <div className="wrapper">
+            <div className="border-circle" id="one"></div>
+            <div className="border-circle" id="two"></div>
+            <div className="background-circle">
+              <div className="triangle-light"></div>
+              <div className="body"></div>
+              <span className="shirt-text">M</span>
+              <span className="shirt-text">♥</span>
+              <span className="shirt-text">R</span>
+              <span className="shirt-text">Y</span>
+              <span className="shirt-text">AM</span>
+              <div className="triangle-dark"></div>
+            </div>
+            <div className="head">
+              <div className="ear" id="left"></div>
+              <div className="ear" id="right"></div>
+              <div className="hair-main">
+                <div className="sideburn" id="left"></div>
+                <div className="sideburn" id="right"></div>
+                <div className="hair-top"></div>
+              </div>
+              <div className="face">
+                <div className="hair-bottom"></div>
+                <div className="nose"></div>
+                <div className="eye-shadow" id="left">
+                  <div className="eyebrow"></div>
+                  <div className="eye"></div>
+                </div>
+                <div className="eye-shadow" id="right">
+                  <div className="eyebrow"></div>
+                  <div className="eye"></div>
+                </div>
+                <div className="mouth"></div>
+                <div className="shadow-wrapper">
+                  <div className="shadow"></div>
+                </div>
+              </div>
+            </div>
+            <span className="music-note" id="one">
+              &#9835;
+            </span>
+            <span className="music-note" id="two">
+              &#9834;
+            </span>
+          </div>
+        </div>
+      )}
     </>
   );
 }
